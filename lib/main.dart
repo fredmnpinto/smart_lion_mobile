c649369346c27bba0f1b46451985a3ada39642f4
Frederico Pinto <fredmnpinto@gmail.com>
2022-03-14 18:35:16 +0000

    Initial Structure of the GetX Pattern

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:smart_lion_mobile/app/routes/app_pages.dart';
 
 import 'app/data/modules/home/page.dart';
 
@@ -12,7 +14,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         // This is the theme of your application.
@@ -27,6 +29,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      getPages: AppPages.pages,
     );
   }
 }
