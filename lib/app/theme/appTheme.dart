b72bc8381c2db676f0355496007eb73d97f481ec
Gabriel Fernandes <force2606@gmail.com>
2022-04-13 19:13:09 +0100

    Created AppTheme

---
+++ lib/app/theme/appTheme.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+final ThemeData appThemeData = ThemeData(
+  primaryColor: Color.fromARGB(255, 35, 206, 107),
+  backgroundColor: Color.fromARGB(255, 247, 249, 249),
+);
